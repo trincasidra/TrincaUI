a26e522eb83ec09f294bc55bff0910a4584b85d5
Robb <robb@MacBook-Pro-de-Robb.local>
2022-01-01 13:25:04 +0100

    Add minimap, buff frame and action button reskin support

--- modules/rBuffFrame.lua
+++ modules/rBuffFrame.lua
@@ -2,31 +2,11 @@ local A, L = ...
 local rBuffFrame = L.rBuffFrame
 
 local function setBuffFrame()
-  if not L.C.rBuffFrame then return end
-
-  local buffFrameConfig = {
-    framePoint      = {"TOPLEFT", UIParent, "TOPLEFT", 10, -10},
-    frameScale      = 1,
-    framePadding    = 5,
-    buttonWidth     = 32,
-    buttonHeight    = 32,
-    buttonMargin    = 5,
-    numCols         = 10,
-    startPoint      = "TOPLEFT",
-    --rowMargin       = 20,
-  }
-  local buffFrame = rBuffFrame:CreateBuffFrame(A, buffFrameConfig)
-
-  local debuffFrameConfig = {
-    framePoint      = {"TOPLEFT", buffFrame, "BOTTOMLEFT", 0, 0 },
-    frameScale      = 1,
-    framePadding    = 5,
-    buttonWidth     = 40,
-    buttonHeight    = 40,
-    buttonMargin    = 5,
-    numCols         = 8,
-    startPoint      = "TOPLEFT",
-  }
-  rBuffFrame:CreateDebuffFrame(A, debuffFrameConfig)
+  if L.C.buffFrameConfig and L.C.buffFrameConfig.enabled then
+    rBuffFrame:CreateBuffFrame(A, L.C.buffFrameConfig)
+  end
+  if L.C.debuffFrameConfig and L.C.debuffFrameConfig.enabled then
+    rBuffFrame:CreateDebuffFrame(A, L.C.debuffFrameConfig)
+  end
 end
 setBuffFrame()
