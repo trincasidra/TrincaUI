32985f8bdb0bfc512ac912ab5a483d9462b34069
Robb <robb@MacBook-Pro-de-Robb.local>
2022-01-11 11:49:42 +0100

    Fixed power text error without power bar. Added support for rFilter. Removed player on combat glow.

--- core/style.lua
+++ core/style.lua
@@ -4,35 +4,48 @@ local oUF = L.oUF
 if not oUF then return end
 
 local function CreateStyle(self)
-  -- Other powers: CHI, RUNES, FUEL, ARCANE_CHARGES, SOUL_SHARDS, AMMOSLOT
-  self.colors.power["PAIN"] = L.C.colors.power.pain
-  self.colors.power["ALTERNATE"] = L.C.colors.power.alternative
-  self.colors.power["COMBO_POINTS"] = L.C.colors.power.combo
-  self.colors.power["FOCUS"] = L.C.colors.power.focus
-  self.colors.power["ENERGY"] = L.C.colors.power.energy
-  self.colors.power["MANA"] = L.C.colors.power.mana
-  self.colors.power["MAELSTROM"] = L.C.colors.power.maelstrom
-  self.colors.power["INSANITY"] = L.C.colors.power.insanity
-  self.colors.power["RUNIC_POWER"] = L.C.colors.power.runic
-  self.colors.power["FURY"] = L.C.colors.power.fury
-  self.colors.power["HOLY_POWER"] = L.C.colors.power.holy
-  self.colors.power["LUNAR_POWER"] = L.C.colors.power.astral
-  self.colors.power["RAGE"] = L.C.colors.power.rage
-  self.colors.reaction[1] = L.C.colors.reaction.harm
-  self.colors.reaction[2] = L.C.colors.reaction.harm
-  self.colors.reaction[3] = L.C.colors.reaction.harm
-  self.colors.reaction[4] = L.C.colors.reaction.neutral
-  self.colors.reaction[5] = L.C.colors.reaction.help
-  self.colors.reaction[6] = L.C.colors.reaction.help
-  self.colors.reaction[7] = L.C.colors.reaction.help
-  self.colors.reaction[8] = L.C.colors.reaction.help
-  self.colors.reaction[8] = L.C.colors.reaction.help
-  self.colors.disconnected = L.C.colors.disconnected
-  self.colors.tapped = L.C.colors.tapped
-  self.colors.threat[0] = L.C.colors.threat.lowthreat
-  self.colors.threat[1] = L.C.colors.threat.highthreat
-  self.colors.threat[2] = L.C.colors.threat.unsafetanking
-  self.colors.threat[3] = L.C.colors.threat.tanking
+  if L.C.colors.disconnected then self.colors.disconnected = L.C.colors.disconnected end
+  if L.C.colors.tapped then self.colors.tapped = L.C.colors.tapped end
+
+  if L.C.colors.power then
+    -- Other self.colors.power not changed: CHI, RUNES, FUEL, ARCANE_CHARGES, SOUL_SHARDS, AMMOSLOT
+    if L.C.colors.power.pain then self.colors.power["PAIN"] = L.C.colors.power.pain end
+    if L.C.colors.power.alternative then self.colors.power["ALTERNATE"] = L.C.colors.power.alternative end
+    if L.C.colors.power.combo then self.colors.power["COMBO_POINTS"] = L.C.colors.power.combo end
+    if L.C.colors.power.focus then self.colors.power["FOCUS"] = L.C.colors.power.focus end
+    if L.C.colors.power.energy then self.colors.power["ENERGY"] = L.C.colors.power.energy end
+    if L.C.colors.power.mana then self.colors.power["MANA"] = L.C.colors.power.mana end
+    if L.C.colors.power.maelstrom then self.colors.power["MAELSTROM"] = L.C.colors.power.maelstrom end
+    if L.C.colors.power.insanity then self.colors.power["INSANITY"] = L.C.colors.power.insanity end
+    if L.C.colors.power.runic then self.colors.power["RUNIC_POWER"] = L.C.colors.power.runic end
+    if L.C.colors.power.fury then self.colors.power["FURY"] = L.C.colors.power.fury end
+    if L.C.colors.power.holy then self.colors.power["HOLY_POWER"] = L.C.colors.power.holy end
+    if L.C.colors.power.astral then self.colors.power["LUNAR_POWER"] = L.C.colors.power.astral end
+    if L.C.colors.power.rage then self.colors.power["RAGE"] = L.C.colors.power.rage end
+  end
+
+  if L.C.colors.reaction then
+    if L.C.colors.reaction.harm then
+      self.colors.reaction[1] = L.C.colors.reaction.harm
+      self.colors.reaction[2] = L.C.colors.reaction.harm
+      self.colors.reaction[3] = L.C.colors.reaction.harm
+    end
+    if L.C.colors.reaction.neutral then self.colors.reaction[4] = L.C.colors.reaction.neutral end
+    if L.C.colors.reaction.help then
+      self.colors.reaction[5] = L.C.colors.reaction.help
+      self.colors.reaction[6] = L.C.colors.reaction.help
+      self.colors.reaction[7] = L.C.colors.reaction.help
+      self.colors.reaction[8] = L.C.colors.reaction.help
+      self.colors.reaction[8] = L.C.colors.reaction.help
+    end
+  end
+
+  if L.C.colors.threat then
+    if L.C.colors.threat.lowthreat then self.colors.threat[0] = L.C.colors.threat.lowthreat end
+    if L.C.colors.threat.highthreat then self.colors.threat[1] = L.C.colors.threat.highthreat end
+    if L.C.colors.threat.tanking then self.colors.threat[2] = L.C.colors.threat.tanking end
+    if L.C.colors.threat.tanking then self.colors.threat[3] = L.C.colors.threat.tanking end
+  end
 
   L.F.SetupFrame(self)
   L.F.SetupHeader(self)
