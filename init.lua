86d88e39d7dc318117a6b814fa2b206738a628d5
trincasidra <trincasidra@users.noreply.github.com>
2022-10-01 21:44:32 +0200

    Updated readme and preview image

--- init.lua
+++ init.lua
@@ -4,12 +4,10 @@ local rLib = L.rLib
 L.addonName       = A
 L.dragFrames      = {}
 L.addonColor      = "00FF3300"
-L.addonShortcut   = "trinca"
 
 local SharedMedia = LibStub("LibSharedMedia-3.0")
 if SharedMedia then
   SharedMedia:Register("font", "Expressway", "Interface\\AddOns\\"..L.addonName.."\\media\\Expressway.ttf")
 end
 
---rLib:CreateSlashCmd(L.addonName, L.addonShortcut, L.dragFrames, L.addonColor)
 print("|c"..L.addonColor..L.addonName.."|r loaded.")
