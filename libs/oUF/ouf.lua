80480d3252fcf2605f11dcc69fd92fa65c22e742
trincasidra <trincasidra@users.noreply.github.com>
2022-11-19 17:18:20 +0100

    Update oUF

--- libs/oUF/ouf.lua
+++ libs/oUF/ouf.lua
@@ -1,6 +1,6 @@
 local parent, ns = ...
 local global = GetAddOnMetadata(parent, 'X-oUF')
-local _VERSION = '11.0.0'
+local _VERSION = '@project-version@'
 if(_VERSION:find('project%-version')) then
 	_VERSION = 'devel'
 end
