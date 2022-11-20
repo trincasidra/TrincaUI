3cb8601e78a57ba8ca3af523e282a21490c5ea30
trincasidra <trincasidra@users.noreply.github.com>
2022-11-20 10:13:00 +0100

    Fixed taint from rChat on edit mode

--- modules/rChat.lua
+++ modules/rChat.lua
@@ -9,8 +9,6 @@
 local A, L = ...
 if not (L.C.chat and L.C.chat.enable) or IsAddOnLoaded("Prat-3.0") then return end
 
-local DefaultSetItemRef = SetItemRef
-
 -----------------------------
 -- Functions
 -----------------------------
@@ -63,32 +61,6 @@ local function OpenTemporaryWindow()
   end
 end
 
---OnMOuseScroll
-local function OnMOuseScroll(self,dir)
-  if(dir > 0) then
-    if(IsShiftKeyDown()) then self:ScrollToTop() else self:ScrollUp() end
-  else
-    if(IsShiftKeyDown()) then self:ScrollToBottom() else self:ScrollDown() end
-  end
-end
-
---we replace the default setitemref and use it to parse links for alt invite and url copy
-function SetItemRef(link, ...)
-  local type, value = link:match("(%a+):(.+)")
-  if IsAltKeyDown() and type == "player" then
-    InviteUnit(value:match("([^:]+)"))
-  elseif (type == "url") then
-    local eb = LAST_ACTIVE_CHAT_EDIT_BOX or ChatFrame1EditBox
-    if not eb then return end
-    eb:SetText(value)
-    eb:SetFocus()
-    eb:HighlightText()
-    if not eb:IsShown() then eb:Show() end
-  else
-    return DefaultSetItemRef(link, ...)
-  end
-end
-
 --AddMessage
 local function AddMessage(self, text, ...)
   --channel replace (Trade and such)
@@ -110,15 +82,6 @@ ChatFontNormal:SetShadowColor(unpack(L.C.chat.dropshadow.color))
 --font size
 CHAT_FONT_HEIGHTS = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 
---tabs
-CHAT_TAB_HIDE_DELAY = 1
-CHAT_FRAME_TAB_NORMAL_MOUSEOVER_ALPHA = 1
-CHAT_FRAME_TAB_NORMAL_NOMOUSE_ALPHA = 0
-CHAT_FRAME_TAB_SELECTED_MOUSEOVER_ALPHA = 1
-CHAT_FRAME_TAB_SELECTED_NOMOUSE_ALPHA = 0
-CHAT_FRAME_TAB_ALERTING_MOUSEOVER_ALPHA = 1
-CHAT_FRAME_TAB_ALERTING_NOMOUSE_ALPHA = 1
-
 --channels
 CHAT_WHISPER_GET              = "From %s "
 CHAT_WHISPER_INFORM_GET       = "To %s "
@@ -138,10 +101,6 @@ CHAT_RAID_LEADER_GET          = "|Hchannel:Raid|hRL.|h %s: "
 CHAT_RAID_WARNING_GET         = "|Hchannel:RaidWarning|hRW.|h %s: "
 CHAT_INSTANCE_CHAT_GET        = "|Hchannel:Battleground|hI.|h %s: "
 CHAT_INSTANCE_CHAT_LEADER_GET = "|Hchannel:Battleground|hIL.|h %s: "
---CHAT_MONSTER_PARTY_GET       = CHAT_PARTY_GET
---CHAT_MONSTER_SAY_GET         = CHAT_SAY_GET
---CHAT_MONSTER_WHISPER_GET     = CHAT_WHISPER_GET
---CHAT_MONSTER_YELL_GET        = CHAT_YELL_GET
 CHAT_FLAG_AFK = "<AFK> "
 CHAT_FLAG_DND = "<DND> "
 CHAT_FLAG_GM = "<[GM]> "
@@ -182,9 +141,6 @@ for i = 1, NUM_CHAT_WINDOWS do
     chatframe.AddMessage = AddMessage
   end
 end
-
---scroll
-FloatingChatFrame_OnMouseScroll = OnMOuseScroll
 
 --temporary chat windows
 hooksecurefunc("FCF_OpenTemporaryWindow", OpenTemporaryWindow)
