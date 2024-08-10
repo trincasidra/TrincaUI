69be58be2daed0842a944cf8fabd1251dcfa010b
Robb <15145418+trincasidra@users.noreply.github.com>
2024-08-10 23:57:52 +0200

    More fixes for tww

--- modules/rSellPoor.lua
+++ modules/rSellPoor.lua
@@ -1,4 +1,3 @@
-
 -- rSellPoor: core
 -- zork, 2018
 
@@ -16,31 +15,31 @@ local stop = true
 local list = {}
 
 local function SellGray()
-  if stop then return end
-  for bag=0,4 do
-    for slot=0,C_Container.GetContainerNumSlots(bag) do
-      if stop then return end
-      local link = C_Container.GetContainerItemLink(bag, slot)
-      if link and select(3, GetItemInfo(link)) == 0 and not list["b"..bag.."s"..slot] then
-        print("Selling", link)
-        list["b"..bag.."s"..slot] = true
-        C_Container.UseContainerItem(bag, slot)
-        C_Timer.After(0.2, SellGray)
-        return
-      end
+    if stop then return end
+    for bag = 0, 4 do
+        for slot = 0, C_Container.GetContainerNumSlots(bag) do
+            if stop then return end
+            local link = C_Container.GetContainerItemLink(bag, slot)
+            if link and select(3, C_Item.GetItemInfo(link)) == 0 and not list["b" .. bag .. "s" .. slot] then
+                print("Selling", link)
+                list["b" .. bag .. "s" .. slot] = true
+                C_Container.UseContainerItem(bag, slot)
+                C_Timer.After(0.2, SellGray)
+                return
+            end
+        end
     end
-  end
 end
 
 
-local function OnEvent(self,event)
-  if event == "MERCHANT_SHOW" then
-    stop = false
-    wipe(list)
-    SellGray()
-  elseif event == "MERCHANT_CLOSED" then
-    stop = true
-  end
+local function OnEvent(self, event)
+    if event == "MERCHANT_SHOW" then
+        stop = false
+        wipe(list)
+        SellGray()
+    elseif event == "MERCHANT_CLOSED" then
+        stop = true
+    end
 end
 
 -----------------------------
